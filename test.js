65de40561f8c34582d6765563399b1a4686a587e
myralt <no@email.com>
2022-02-10 13:46:39 +0100

    Added content type of response

--- test.js
+++ test.js
@@ -7,4 +7,7 @@ console.log("Server running at http://127.0.0.1:8080/");
 
 function newClientCallback(request, response) {
   console.log("New Client Connection");
+  console.log(request.rawHeaders);
+  response.writeHead(200, {'content-type': 'text/html'});
+  response.end("<h1>Hello, world!</h1>");
 }
