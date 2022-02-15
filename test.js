42d820df8750e518d387f56c154e5022e4769085
myralt <no@email.com>
2022-02-15 09:41:19 +0100

    Separated services into conditionals

--- test.js
+++ test.js
@@ -9,21 +9,44 @@ console.log("Server running at http://127.0.0.1:8080/");
 function newClientCallback(request, response) {
   n++;
   console.log(`Number of connections: ${n}`);
+
+  let date = new Date();
+  let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let links = ["/time", "/date", "/header"];
+  
   if (request.url === "/time") {
     response.writeHead(200, {'content-type': 'text/html'});
-    let date = new Date();
     response.end(`<h1>${date.getHours()}:${date.getMinutes()}</h1>`);
   }
-
-  let list = "<ul>";
-  request.rawHeaders.forEach((props, i) => {
-    if (i % 2) list += props + "</li>";
-    else list += "<li>" + props + ": ";
+  else if (request.url === "/date"){
+    let month = date.getMonth() + 1;
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.end(`<h1>${week[date.getDay()]} ${date.getDate()}/${ month < 10 ? "0" + month : month }/${date.getFullYear()}</h1>`);
   }
-  );
-  list += "</ul>";
-  response.writeHead(200, {'content-type': 'text/html'});
-  response.end(list);
+  else if (request.url === "/header"){
+    let list = "<ul>";
+    request.rawHeaders.forEach((props, i) => {
+      if (i % 2) list += props + "</li>";
+      else list += "<li>" + props + ": ";
+    }
+    );
+    list += "</ul>";
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.end(list);
+  }
+  else {
+    let list = "<ul>";
+    let message = "<h1>Welcome!</h1><p>To access the different services provided enter at the end of the url either:</p>";
+    links.forEach((link) => {
+      list += "<li>" + link + "</li>";
+    });
+    list += "</ul>";
+    message += list;
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.end(message);
+  }
+
   // console.log(request);
   // console.log(request.rawHeaders);
   // console.log(request.url);
